54183b577421c67a66c56ae2e8215207334d0553
Doivssel <coindet.jeremy@gmail.com>
2024-07-05 15:24:49 +0200

    Update snake.py
    Create a game class

--- snake.py
+++ snake.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 class Snake(object):
     
-    def __init__(self,map):
+    def __init__(self,game,map):
         """
         Parameters:
         map: instance of the class Map
@@ -15,16 +15,35 @@ class Snake(object):
         and the first body part(the head).
 
         """
-        self.direction_ud=0
-        self.direction_lr=1
-        self.posX=0
-        self.posY=0
+        self.direction_lr,self.direction_ud=self.init_dir(map)
+        self.posX,self.posY=self.init_pos(map)
         self.score=0
+        self.game=game
         self.body=np.array([[self.posX,self.posY]])
         map.create_body_part(self)
         map.map[self.posY,self.posX]=1
         map.add_apple()
 
+    def init_pos(self,map):
+        i=np.random.randint(1,map.number_col-1)
+        j=np.random.randint(1,map.number_row-1)
+        return(i,j)
+    
+    def init_dir(self,map):
+        dir=np.random.randint(0,4)
+        if(dir==0):
+            self.direction_lr=1
+            self.direction_ud=0
+        elif(dir==1):
+            self.direction_lr=-1
+            self.direction_ud=0
+        elif(dir==2):
+            self.direction_ud=-1
+            self.direction_lr=0
+        elif(dir==3):
+            self.direction_ud=1
+            self.direction_lr=0
+        return(self.direction_lr,self.direction_ud)
 
     def move(self,map):
 
@@ -44,11 +63,11 @@ class Snake(object):
         self.posY+=self.direction_ud
         self.posX+=self.direction_lr
         if(self.posX>=map.number_col or self.posY>=map.number_row or self.posX<0 or self.posY<0):
-            lose()
+            self.game.lose()
         else:
             map_value=map.map[self.posY,self.posX]
             if(map_value==1):
-                lose()
+                self.game.lose()
             elif(map_value==0):
                 map.map[self.posY,self.posX]=1
                 self.body=np.vstack((self.body,[self.posX,self.posY]))
@@ -62,15 +81,15 @@ class Snake(object):
                 map.map[self.posY,self.posX]=1
                 self.body=np.vstack((self.body,[self.posX,self.posY]))
                 map.create_body_part(self)
-                t_canvas.delete("temp_text")
-                t_canvas.create_text(300,40,text="current score : "+str(self.score),font=("Helvetica 25 bold"),tags="temp_text")
+                self.game.t_canvas.delete("temp_text")
+                self.game.t_canvas.create_text(300,40,text="current score : "+str(self.score),font=("Helvetica 25 bold"),tags="temp_text")
 
 
 
 
 class Map(object):
 
-    def __init__(self,row,col):
+    def __init__(self,game,row,col):
         """
         Parameters:
         row: number of rows wanted for the grid
@@ -95,7 +114,7 @@ class Map(object):
         self.map=np.zeros((row,col))
         self.grid_Y=np.linspace(0,600,self.number_row+1)[0:self.number_row]
         self.grid_X=np.linspace(0,600,self.number_col+1)[0:self.number_col]
-
+        self.game=game
 
     def create_body_part(self,snake):
         """
@@ -106,7 +125,7 @@ class Map(object):
         Update the graphical representation of the snake by adding
         a body part.
         """
-        canvas.create_rectangle(self.grid_X[snake.posX],self.grid_Y[snake.posY],self.grid_X[snake.posX]+600/self.number_col,self.grid_Y[snake.posY]+600/self.number_row,fill="blue")
+        self.game.canvas.create_rectangle(self.grid_X[snake.posX],self.grid_Y[snake.posY],self.grid_X[snake.posX]+600/self.number_col,self.grid_Y[snake.posY]+600/self.number_row,fill="blue")
 
     def delete_body_part(self,snake):  
         """
@@ -120,7 +139,7 @@ class Map(object):
         Note:
         Could be fuse with the above function but I find it clearer this way
         """
-        canvas.create_rectangle(self.grid_X[snake.body[0,0]],self.grid_Y[snake.body[0,1]],self.grid_X[snake.body[0,0]]+600/self.number_col,self.grid_Y[snake.body[0,1]]+600/self.number_row,fill="black")
+        self.game.canvas.create_rectangle(self.grid_X[snake.body[0,0]],self.grid_Y[snake.body[0,1]],self.grid_X[snake.body[0,0]]+600/self.number_col,self.grid_Y[snake.body[0,1]]+600/self.number_row,fill="black")
 
     def add_apple(self):
         """
@@ -134,68 +153,81 @@ class Map(object):
         indice=np.transpose(np.nonzero(self.map.T==0))
         self.rdm_pos=indice[np.random.randint(0,len(indice))]
         self.map[self.rdm_pos[1],self.rdm_pos[0]]=2
-        canvas.create_rectangle(self.grid_X[self.rdm_pos[0]],self.grid_Y[self.rdm_pos[1]],self.grid_X[self.rdm_pos[0]]+600/self.number_col,self.grid_Y[self.rdm_pos[1]]+600/self.number_row,fill="red")
+        self.game.canvas.create_rectangle(self.grid_X[self.rdm_pos[0]],self.grid_Y[self.rdm_pos[1]],self.grid_X[self.rdm_pos[0]]+600/self.number_col,self.grid_Y[self.rdm_pos[1]]+600/self.number_row,fill="red")
 
 
+class Game(object):
+    
+    def __init__(self,root,dim_map,speed):
+        """
+        Parameters:
+        
+        
+        """
+        self.dim_map=dim_map
+        self.speed=speed
+        self.root=root
+        self.root.title("Snake")
+        self.root.geometry("600x700")
 
-def playing():
-    snake.move(map)
-    root.after(100,playing)
+        self.canvas=tk.Canvas(self.root,width=600,height=600,bg="black")
+        self.canvas.pack()
 
-def lose():
-    """
-    Parameters:
-    None
+        self.t_canvas=tk.Canvas(self.root,width=600,height=100,bg="yellow")
+        self.t_canvas.pack()
 
-    Goal:
-    When the snake does an error the canvas is cleared. And 
-    a new instance of snake and map are created as way to set the game back
-    to zero.
-    """
-    global snake
-    global map
-    canvas.delete("all")
-    t_canvas.delete("all")
-    map=Map(15,15)
-    snake=Snake(map)
+        self.map=Map(self,dim_map,dim_map)
+        self.snake=Snake(self,self.map)
 
+        self.root.bind("<Key>",self.key)
+        self.playing()
 
-def key(event):
-    """
-    Parameters:
-    event: just a keyword for the tkinter event 
+    def key(self,event):
+        """
+        Parameters:
+        event: just a keyword for the tkinter event 
 
-    Goal:
-    Change the direction of the snake by modifying snake direction_lr/ud.
-    The changement is done only if the right key is pressed when the snake
-    has the right orientation. For example it cannot go the right if he is going to the left.
-    It has to go down or up before.
-    """
-    key_pressed=event.keysym
-    if(key_pressed=="Right" and snake.direction_lr==0):
-        snake.direction_lr=1
-        snake.direction_ud=0
-    if(key_pressed=="Left" and snake.direction_lr==0):
-        snake.direction_lr=-1
-        snake.direction_ud=0
-    if(key_pressed=="Up" and snake.direction_ud==0):
-        snake.direction_ud=-1
-        snake.direction_lr=0
-    if(key_pressed=="Down" and snake.direction_ud==0):
-        snake.direction_ud=1
-        snake.direction_lr=0
+        Goal:
+        Change the direction of the snake by modifying snake direction_lr/ud.
+        The changement is done only if the right key is pressed when the snake
+        has the right orientation. For example it cannot go the right if he is going to the left.
+        It has to go down or up before.
+        """
+        key_pressed=event.keysym
+        if(key_pressed=="Right" and self.snake.direction_lr==0):
+            self.snake.direction_lr=1
+            self.snake.direction_ud=0
+        if(key_pressed=="Left" and self.snake.direction_lr==0):
+            self.snake.direction_lr=-1
+            self.snake.direction_ud=0
+        if(key_pressed=="Up" and self.snake.direction_ud==0):
+            self.snake.direction_ud=-1
+            self.snake.direction_lr=0
+        if(key_pressed=="Down" and self.snake.direction_ud==0):
+            self.snake.direction_ud=1
+            self.snake.direction_lr=0
+
+    def lose(self):
+        """
+        Parameters:
+        None
+
+        Goal:
+        When the snake does an error the canvas is cleared. And 
+        a new instance of snake and map are created as way to set the game back
+        to zero.
+        """
+        self.canvas.delete("all")
+        self.t_canvas.delete("all")
+        self.map=Map(self,self.dim_map,self.dim_map)
+        self.snake=Snake(self,self.map)
+
+    def playing(self):
+        self.snake.move(self.map)
+        self.root.after(self.speed,self.playing)
 
 root=tk.Tk()
-root.geometry("600x700")
 
-canvas=tk.Canvas(root,width=600,height=600,bg="black")
-canvas.pack()
-t_canvas=tk.Canvas(root,width=600,height=100,bg="yellow")
-t_canvas.pack()
+game=Game(root,20,80)
 
-map=Map(15,15)
-snake=Snake(map)
-
-root.bind("<Key>",key)
-playing()
-root.mainloop()
+game.root.mainloop()
